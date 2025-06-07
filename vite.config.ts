6517198e89e8df04b62af5066a4ef4b1e84ace38
ramzan_abdulnur <gordeew.r2015@yandex.ru>
2025-06-07 20:10:35 +0300

    rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1500
+  }
 })
